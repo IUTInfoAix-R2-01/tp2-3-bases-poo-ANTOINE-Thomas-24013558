397973af726ded45753282b8671c41bd93bcfa24
a24013558 <a24013558@A-F005586.salsa.univ-amu.fr>
2025-01-31 17:25:56 +0100

    Ajout de l'exam du 31/01/2025 pour la classe Prof avec TestProf

---
+++ TP2/Prof/Personne.java
@@ -0,0 +1,28 @@
+package Prof;
+
+public class Personne {
+	public String Nom;
+	public String Prenom;
+	
+	public Personne(String Prenom, String Nom) {
+		this.Nom = Nom;
+		this.Prenom = Prenom;
+	}
+	public Personne(String Prenom) {
+		this.Prenom = Prenom;
+	}
+	public String getNom() {
+		return Nom;
+	}
+	public String getPrenom() {
+		return Prenom;
+	}
+	public String toString() {
+		if (Nom == null) {
+			return Prenom;
+		}
+		else {
+		return Prenom + " " + Nom;
+		}
+	}
+}
---
+++ TP2/Prof/testPersonne.java
@@ -0,0 +1,10 @@
+package Prof;
+
+public class testPersonne {
+	public static void main(String[] args) {
+		Personne p1 = new Personne("Laurence", "Ago");
+		System.out.println(p1);
+		Personne p2 = new Personne("Lolo");
+		System.out.println(p2);
+	}
+}
